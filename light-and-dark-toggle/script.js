ae546ad045dad9941ea91cae5b41e75ddf612cd1
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-02-23 16:26:38 +0530

    added basic fuctionality reamining things tommorow

---
+++ light-and-dark-toggle/script.js
@@ -0,0 +1,8 @@
+const changeThemeBtn=document.querySelector(".change-theme-btn");
+const body=document.querySelector('body');
+
+changeThemeBtn.addEventListener('click',()=>{
+    if{body.classList.contains}
+})
+
+//2:49 onwards
